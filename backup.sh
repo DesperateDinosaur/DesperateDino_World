f044c36811d9a6307db976d85c4ecb998881ef93
Gabriel Internoscia <gabriel_internoscia@outlook.com>
2026-01-01 14:16:05 -0500

    Thu 01 Jan 2026 14:16:05 EST

--- backup.sh
+++ backup.sh
@@ -3,14 +3,10 @@ repo_dir=$(pwd)
 world_dir='../minecraftServer/world/'
 
 cd $repo_dir
+echo $(pwd)
 git pull origin main -q
-result=$(git status --porcelain | wc -1)
 
-if [[ $result -eq 0 ]] ; then
-	exit 0
-
-
-fi
-
-zip -r ./world_backup.zip $world_dir
-git add .; git commit -q -m "$(date +"%c")"; git push origin main -q
+# zip -r ./world_backup.zip $world_dir
+git add . && \
+git commit -q -m "$(date +"%c")" && \
+git push
