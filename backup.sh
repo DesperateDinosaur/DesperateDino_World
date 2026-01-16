8f6ef362eef21143cca78424bd716cbf0b88c463
Gabriel Internoscia <gabriel_internoscia@outlook.com>
2026-01-16 02:03:23 -0500

    Fri 16 Jan 2026 02:03:23 EST

--- backup.sh
+++ backup.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 repo_dir='/home/desperatedinosaur/DesperateDino_World/'
 world_dir='/home/desperatedinosaur/minecraftServer/world/'
+backup_world_dir='/home/desperatedinosaur/DesperateDino_World/world_backup'
 
 cd $repo_dir
 
 git pull origin main -q
 
-rm -r ./world_backup
-cp -r $world_dir ./world_backup
+rm -r $backup_world_dir
+cp -r $world_dir $backup_world_dir
 # zip -r ./world_backup.zip $world_dir
 
 git add . && \
